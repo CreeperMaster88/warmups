f04c09d55f8645e0decb1c8d1a5d3f72602e6ea8
Eliot <etroop719@gmail.com>
2018-08-18 09:58:47 -0700

    This warmup uses my custom 'Contains' function to sort an array.

---
+++ 81818/81818/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _81818
+{
+    class Program
+    {
+        //take in an array to search through
+        //take in an item to search for
+        //search the array for the item
+        //if it is there return true. if not return false
+        static bool Contains(int[] numbers, int searchFor)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == searchFor)
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        //create an array and fill it with random numbers
+        //ask the user to enter a number to check for
+        //tell the user whether the array contains the number or not
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user, I will produce a random array with numbers between 1 and 100, and you can ask if a number is in there or not.");
+            Random gen = new Random();
+            int[] numbers = new int[gen.Next(20,100)];
+            Console.WriteLine("What number do you want to look for between 0 and 100?");
+            int guess = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] += gen.Next(0, 100);
+            }
+
+            if (Contains(numbers, guess))
+            {
+                Console.WriteLine($"Guess What? {guess} is in there! Good Job.");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry. {guess} doesn't exist in the array.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
